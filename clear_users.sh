f72e9dfbab9ef379cc576d89b9a2a4dda065a247
vboxuser <vboxuser@Ubuntu.tntech.edu>
2026-04-14 11:57:56 -0500

    Updated main.sh and user_manual.txt

--- clear_users.sh
+++ clear_users.sh
@@ -3,12 +3,13 @@
 source ./scraper.sh
 source ./user_management.sh
 
-names=$(scrape)
+clear_users() {
+	names=$(scrape)
 
 
-while read -r line; do
-	first=$(echo "$line" | awk '{print $1}')
-        last=$(echo "$line" | awk '{print $NF}')
-        delete_user "$first" "$last"
-done <<< "$names"
-
+	while read -r line; do
+		first=$(echo "$line" | awk '{print $1}')
+        	last=$(echo "$line" | awk '{print $NF}')
+        	delete_user "$first" "$last"
+	done <<< "$names"
+}
--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 source ./scraper.sh
 source ./parser.sh
 source ./user_management.sh
+source ./clear_users.sh
 source "$(dirname "$0")/license.sh"
 #enforce_license
 
@@ -10,10 +11,25 @@ echo "Welcome. Please choose an action:"
 echo "1. Make users from TNTech Computer Science faculty website"
 echo "2. Delete a user from the database"
 echo "3. Manually add a new user"
-read choice
+echo "4. Delete all CS faculty users from the database"
 
 #input validation
+while :; do
+    read choice
 
+    # Step 1: Validate it's a number
+    if ! [[ "$choice" =~ ^-?[0-9]+$ ]]; then
+        echo "Error: Not a valid integer."
+        continue
+    fi
+    
+    # Step 2: Validate the range
+    if (( $choice >= 1 && $choice <= 4 )); then
+        break
+    else
+        echo "Error: Number must be between 1 and 4."
+    fi
+done
 
 #Option 1
 if ((choice == 1)); then
@@ -34,4 +50,9 @@ fi
 if ((choice == 3)); then
 	read -p "Who would you like to add:" first last
 	create_user "$first" "$last"
+fi
+
+#option 4
+if ((choice == 4)); then
+	clear_users
 fi
